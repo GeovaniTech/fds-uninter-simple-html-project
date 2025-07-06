9bfe6dd8d8a4468abea16a53f5507180505292ea
Geovani Debastiani <geovanidebastiani32@gmail.com>
2025-07-06 20:30:27 -0300

    feat: Adding files to repository

---
+++ script.js
@@ -0,0 +1,15 @@
+// Adicionar o evento de hover ao link para mudar de cor
+let linkReceita = document.getElementById('link-receita')
+
+linkReceita.addEventListener("mouseover", e => {
+    linkReceita.style.color = 'red'
+})
+
+// Ajusta a visibilidade da curiosidade
+function onShowMensagem() {
+    document.querySelector("#mensagem-curiosidade").style.display = 'block';
+
+
+    const buttonShowMessage = document.getElementById('button-mostrar-mensagem').appendChild(p)
+}
+
